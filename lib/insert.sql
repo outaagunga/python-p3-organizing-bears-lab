39a3bd6f6bf83faaf9dfa7d3e842f816ee15511f
outaagunga <jeconiaagunga@gmail.com>
2023-09-03 12:41:15 +0300

    Completed the lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Bear1', 5, 'Male', 'Brown', 'Calm', 1),
+    ('Bear2', 8, 'Female', 'Black', 'Energetic', 0),
+    ('Bear3', 3, 'Male', 'White', 'Aggressive', 1),
+    ('Bear4', 10, 'Female', 'Brown', 'Calm', 1),
+    ('Bear5', 6, 'Male', 'Black', 'Energetic', 0),
+    ('Bear6', 7, 'Female', 'White', 'Aggressive', 1),
+    (NULL, 4, 'Male', 'Brown', 'Calm', 0),
+    ('Bear8', 2, 'Female', 'Black', 'Energetic', 1);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,57 @@
+# select_all_female_bears_return_name_and_age = """
+#     Write your SQL query here
+# """
+
+# select_all_bears_names_and_orders_in_alphabetical_order = """
+#     Write your SQL query here
+# """
+
+# select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
+#     Write your SQL query here
+# """
+
+# select_oldest_bear_and_returns_name_and_age = """
+#     Write your SQL query here
+# """
+# select_youngest_bear_and_returns_name_and_age = """
+#     Write your SQL query here
+# """
+# sql_queries.py
+
+# Select all female bears and return their name and age
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE sex = 'F';
 """
 
+# Select all bears' names and order them alphabetically
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+SELECT name
+FROM bears
+ORDER BY name;
 """
 
+# Select all bears' names and ages that are alive and order them from youngest to oldest
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE alive = 1
+ORDER BY age;
 """
 
+# Select the oldest bear and return its name and age
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+ORDER BY age DESC
+LIMIT 1;
 """
+
+# Select the youngest bear and return its name and age
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+ORDER BY age
+LIMIT 1;
 """
